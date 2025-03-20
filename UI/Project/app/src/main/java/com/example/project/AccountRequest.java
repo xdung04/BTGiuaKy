3fcc3eedaf61a12580a2793fc436fe139d88e71f
NguyenVanAn-200704 <nva2004gl81@gmail.com>
2025-03-20 15:17:38 +0700

    sd

---
+++ UI/Project/app/src/main/java/com/example/project/AccountRequest.java
@@ -0,0 +1,61 @@
+package com.example.project;
+
+public class AccountRequest {
+    String username;
+    String email;
+    String password;
+    String avatar;
+    String gender;
+
+    public AccountRequest(String username, String email, String password, String avatar, String gender) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+        this.gender = gender;
+    }
+
+    public AccountRequest() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
