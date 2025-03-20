56f25b8f11b4f2129ee88684e26db3d8122aa1be
xdung04 <157205064+xdung04@users.noreply.github.com>
2025-03-20 15:33:53 +0700

    c

---
+++ API/APIGiuaKy/src/main/java/com/example/APIGiuaKy/DTO/ProductResponse.java
@@ -0,0 +1,18 @@
+package com.example.APIGiuaKy.DTO;
+
+public class ProductResponse {
+    private String avatar;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public ProductResponse(String avatar) {
+        this.avatar = avatar;
+    }
+}
+//Lưu Xuân Dũng
